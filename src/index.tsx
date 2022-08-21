2f9cb7c16b8d0df403218037708cd55ce5f82842
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2022-08-21 17:09:49 +0300

    constructor 2nd try

--- src/index.tsx
+++ src/index.tsx
@@ -45,27 +45,27 @@ root.render(
                       thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
                   />
                   <ConstructorElement
-                      text="Краторная булка N-200i (верх)"
+                      text="Говяжий метеорит (отбивная)"
                       price={50}
                       thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
                   />
                   <ConstructorElement
-                      text="Краторная булка N-200i (верх)"
+                      text="Биокотлета из марсианской Магнолии"
                       price={50}
                       thumbnail={'https://code.s3.yandex.net/react/code/meat-01.png'}
                   />
                   <ConstructorElement
-                      text="Краторная булка N-200i (верх)"
+                      text="Соус Spicy-X"
                       price={50}
                       thumbnail={'https://code.s3.yandex.net/react/code/sauce-02.png'}
                   />
                   <ConstructorElement
-                      text="Краторная булка N-200i (верх)"
+                      text="Мясо бессмертных моллюсков Protostomia"
                       price={50}
                       thumbnail={'https://code.s3.yandex.net/react/code/meat-02.png'}
                   />
                   <ConstructorElement
-                      text="Краторная булка N-200i (верх)"
+                      text="Соус традиционный галактический"
                       price={50}
                       thumbnail={'https://code.s3.yandex.net/react/code/sauce-03.png'}
                   />
@@ -79,8 +79,6 @@ root.render(
               </div>
           </ul>
           <Button type="primary" size="medium" onClick={consoleR}></Button>
-
-
 
       </section>
 
