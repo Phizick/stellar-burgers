f2d9659d080135092768144f0abce988d11f02ca
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2022-08-21 16:20:42 +0300

    constructor 1st try

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Logo, BurgerIcon, ListIcon, ProfileIcon  } from "@ya.praktikum/react-developer-burger-ui-components"
+import { Logo, BurgerIcon, ListIcon, ProfileIcon, ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -29,12 +29,61 @@ root.render(
                     <p className={'text text_type_main-default text_color_inactive pl-2'}>Личный кабинет</p>
                 </li>
             </ul>
-
-
         </nav>
-
-
     </header>
+      <section className={'constructor-section'}>
+          <ul className={'constructor-ul'}>
+              <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+                  <ConstructorElement
+                      type="top"
+                      isLocked={true}
+                      text="Краторная булка N-200i (верх)"
+                      price={200}
+                      thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
+                  />
+                  <ConstructorElement
+                      text="Краторная булка N-200i (верх)"
+                      price={50}
+                      thumbnail={'https://code.s3.yandex.net/react/code/meat-04.png'}
+                  />
+                  <ConstructorElement
+                      text="Краторная булка N-200i (верх)"
+                      price={50}
+                      thumbnail={'https://code.s3.yandex.net/react/code/meat-01.png'}
+                  />
+                  <ConstructorElement
+                      text="Краторная булка N-200i (верх)"
+                      price={50}
+                      thumbnail={'https://code.s3.yandex.net/react/code/sauce-02.png'}
+                  />
+                  <ConstructorElement
+                      text="Краторная булка N-200i (верх)"
+                      price={50}
+                      thumbnail={'https://code.s3.yandex.net/react/code/meat-02.png'}
+                  />
+                  <ConstructorElement
+                      text="Краторная булка N-200i (верх)"
+                      price={50}
+                      thumbnail={'https://code.s3.yandex.net/react/code/sauce-03.png'}
+                  />
+                  <ConstructorElement
+                      type="bottom"
+                      isLocked={true}
+                      text="Краторная булка N-200i (низ)"
+                      price={200}
+                      thumbnail={'https://code.s3.yandex.net/react/code/bun-01.png'}
+                  />
+              </div>
+          </ul>
+
+
+      </section>
+
+
+
+
+
+
 
 
 
