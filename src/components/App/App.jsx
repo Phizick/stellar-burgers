8027efc033082da5cfa511f7c85d9ec0223b23b4
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2022-08-29 15:46:14 +0300

    added Api res

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,21 +6,24 @@ import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor'
 
 const App = () => {
-    // React.useEffect(() => {
-    //     const getResponse = async () => {
-    //         try {
-    //             const res = await fetch('https://norma.nomoreparties.space/api/ingredients')
-    //             if (res.status === 200) {
-    //                 const resData = await res.json()
-    //                 console.log(resData)
-    //             } throw new Error('error')
-    //         } catch (err) {
-    //             console.log(err)
-    //         }
-    //     }
-    //     getResponse()
-    //         .then()
-    // }, [])
+    const [ingredients, setIngredients] = React.useState([])
+     React.useEffect(() => {
+        const getResponse = async () => {
+             try {
+                 const res = await fetch('https://norma.nomoreparties.space/api/ingredients')
+                 if (res.status === 200) {
+                    const resData = await res.json()
+                     setIngredients(resData.data)
+                 } throw new Error('error')
+            } catch (err) {
+                 console.log(err)
+             }
+         }
+         getResponse()
+             .then()
+    }, [])
+
+    console.log(ingredients)
 
     return (
         <div>
