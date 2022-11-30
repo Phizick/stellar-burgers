73f5d95f63bc8021bb17a802c900dea2f49837d0
Denis Kraev <90326255+Phizick@users.noreply.github.com>
2022-11-30 22:21:07 +0300

    added LoginPage without form component

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {useDispatch} from "react-redux";
+
+export const LoginPage = () => {
+    const dispatch = useDispatch();
+    const loginInpurRef = React.useRef(null);
+
+}
